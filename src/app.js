8d4275ad18277c00234f8fcb047988fe498ed32a
vedant <kushal.m@turtlemint.com>
2018-12-11 19:07:53 +0530

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,24 +1,3 @@
 import React from 'react';
-import { componentFromStream , createEventHandler } from "recompose";
-import { combineLatest } from "rxjs";
-import { map, tap } from "rxjs/operators";
-import AddTodo from './AddTodo/AddTodo';
-
-import Todos from './Todos/todos';
-// import User from './User/User';
-
-const App = componentFromStream(prop$ => {
-    const { handler: onAddHandler, stream: clickStream } = createEventHandler();
-    const value$ = clickStream.pipe( tap(input =>
-        console.warn(input)
-        ));
-
-    return combineLatest(prop$, value$).pipe(map(([props, value]) => <div>
-          <AddTodo onAddHandler={onAddHandler}  />
-          {/* <input placeholder="Add TODO" onChange={handler} /> */}
-          <Todos todo={value} />
-        </div>));
-});
-
-
+const App = () => {}
 export default App;
